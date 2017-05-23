1733731ff148d2f7c1e73137a75d0a5838ab8365
sandersson17 <sebastianandersson@students.berkeley.net>
2017-05-23 11:02:53 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+/**
+ * Created by h205p3 on 5/22/17.
+ */
+companies = ["Google", "Uber", "Tinder", "Grindr", "eBay"];
+clients = ["stoners", "old people", "ex-cons"];
+
+function generate(){
+    var client = clients[Math.floor(Math.random() * (clients.length+1))];
+    var company = companies[Math.floor(Math.random() * (companies.length+1))]
+
+
+};
